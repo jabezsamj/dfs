e51368882dcc4a2ab1ea4defc8dcba21e2f02bf6
johnsamjohn <jabezsamj@gmail.com>
2017-11-21 20:25:22 +0000

    Adding multi-threading code

---
+++ RunMultiThreadServer.java
@@ -0,0 +1,24 @@
+package servers;
+
+
+public class RunMultiThreadServer {
+	
+    public static void main (String args[])
+    {
+    	
+    	MultiThreadedServer server = new MultiThreadedServer(9000);
+    	new Thread(server).start();
+
+    	try {
+    	    Thread.sleep(20 * 1000);
+    	} catch (InterruptedException e) {
+    	    e.printStackTrace();
+    	}
+    	System.out.println("Stopping Server");
+    	server.stop();
+    	
+    }
+	
+	
+
+}
