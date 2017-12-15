37f4dc798e306c784b8fa588d5525d1a77b56ce2
johnsamjohn <jabezsamj@gmail.com>
2017-12-15 22:33:02 +0000

    Deleting Extra work

--- java-rmi-example/RMIServerIntf.java
+++
@@ -1,8 +0,0 @@
-package com.andrewdemaria.test.rmi.remote_test;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RMIServerIntf extends Remote {
-	public String getMessage() throws RemoteException;
-}
